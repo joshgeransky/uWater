8bcb44d86caf76d3e1b6fa4258f56baed675513c
joshgeransky <jgeransky91@gmail.com>
2019-04-05 10:48:43 -0700

    Fixing conflicts

---
+++ setup_firebase_cameron.js
@@ -0,0 +1,15 @@
+(function(){
+    var config = {
+    apiKey: "AIzaSyCdJX7MYrsGJqZc2A68_sL4962jeiRu5MQ",
+    authDomain: "yyyyyy-8ed37.firebaseapp.com",
+    databaseURL: "https://yyyyyy-8ed37.firebaseio.com",
+    projectId: "yyyyyy-8ed37",
+    storageBucket: "yyyyyy-8ed37.appspot.com",
+    messagingSenderId: "657309073605"
+  };
+})
+<script>
+  // Initialize Firebase
+  
+  firebase.initializeApp(config);
+</script>
